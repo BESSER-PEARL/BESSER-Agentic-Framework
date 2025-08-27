71f8ad4016475919a7b12a1daf16087d2b5e2bb0
Natarajan Chidambaram <48755692+natarajan-chidambaram@users.noreply.github.com>
2025-08-27 19:04:37 +0200

    my test file for checking if everything works

--- besser/agent/test/read_agent_card.py
+++ besser/agent/test/read_agent_card.py
@@ -1,10 +1,26 @@
+import sys
+sys.path.append("C:/Users/chidambaram/Downloads/GitHub/BESSER-Agentic-Framework_Natarajan")
+
 import requests
+from aiohttp import web
+
 from besser.agent.core.agent import Agent
+from besser.agent.platforms.a2a.a2a_platform import A2APlatform
+from besser.agent.platforms.a2a.server import create_app
 
-response = requests.get("http://localhost:8000/agent-card", verify=False)
+agent = Agent('TestAgent')
 
-if response.status_code == 200:
-    print("Agent Card:")
-    print(response.json())
-else:
-    print(f"Failed to fetch agent card: {response.status_code} - {response.text}")
+a2a_platform = agent.use_a2a_platform()
+
+# a2a_platform.agent_card.capabilities = ['print text']
+a2a_platform.add_capabilities('print back')
+a2a_platform.add_descriptions(['under development'])
+a2a_platform.add_examples(['python read_agent_card.py'])
+
+a2a_platform.router.register('ping', lambda: 'pong')
+a2a_platform.router.register("echo", lambda msg: f"echo: {msg}")
+
+print(a2a_platform.get_agent_card())
+
+app = create_app(a2a_platform)
+web.run_app(app, port=8000)
