d882fa8b3adfcead34e266f443beee09ad19dda9
Natarajan CHIDAMBARAM <natarajan.chidambaram@list.lu>
2025-09-02 14:42:02 +0200

    Reordering code block in example
    Moved add capabilities, description and so on to the bottom, as it makes sense to add them only after the full agent is developed. Also, the methods are captured from the script directly.

--- besser/agent/test/examples/a2a_agent.py
+++ besser/agent/test/examples/a2a_agent.py
@@ -15,21 +15,23 @@ agent = Agent('TestAgent')
 
 a2a_platform = agent.use_a2a_platform()
 
-# a2a_platform.agent_card.capabilities = ['print text']
-a2a_platform.add_capabilities('print back')
-a2a_platform.add_descriptions(['under development'])
-a2a_platform.add_examples(['python read_agent_card.py'])
-
 async def echo(msg: str):
+    if not isinstance(msg, str):
+        raise ValueError("msg must be a string")
+    
     print(f"message is: {msg} before start")
     await asyncio.sleep(60)
     return f"message is: {msg} after ending"
 
-a2a_platform.router.register("agent.card.get", lambda: a2a_platform.get_agent_card())
-a2a_platform.router.register("capabilities.list", lambda: a2a_platform.agent_card.capabilities)
-a2a_platform.router.register("echo", echo)
+async def failing_method():
+    raise Exception('exception_raised')
+
+a2a_platform.router.register("get_agent_card", lambda: a2a_platform.get_agent_card())
+a2a_platform.router.register("agent_capabilities", lambda: a2a_platform.agent_card.capabilities)
+a2a_platform.router.register("echo_message", echo)
 a2a_platform.router.register("task_create", create_task)
 a2a_platform.router.register("task_status", get_status)
+a2a_platform.router.register("failing_method", failing_method)
 
 async def create_task_and_run(method: str, params: dict, router):
     task_info = create_task(method, params)
@@ -42,6 +44,11 @@ async def rpc_create_task(method: str, params: dict):
 a2a_platform.router.register("create_task_and_run", rpc_create_task)
 
 # print(a2a_platform.get_agent_card())
+# a2a_platform.agent_card.capabilities = ['print text']
+a2a_platform.add_capabilities('Prints the entered message')
+a2a_platform.add_descriptions(['Waits for 60 seconds and then provides the entered message'])
+a2a_platform.add_methods(list(a2a_platform.router.methods.keys()))
+a2a_platform.add_examples(['curl -X POST http://localhost:8000/a2a -H "Content-Type: application/json" -d "{\"jsonrpc\":\"2.0\",\"method\":\"create_task_and_run\",\"params\":{\"method\":\"echo_message\",\"params\":{\"msg\":\"hellloooo1\"}},\"id\":1}"'])
 
 app = create_app(a2a_platform)
 web.run_app(app, port=8000)
