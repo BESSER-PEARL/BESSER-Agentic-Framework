e2c8f4ba6d54b6cda3b34dde7a5d918a069abf20
mgv99 <gomezmarcos99@gmail.com>
2026-04-23 17:14:02 +0200

    fix config.yaml reference in example agents

--- baf/test/examples/llm_agent.py
+++ baf/test/examples/llm_agent.py
@@ -19,7 +19,7 @@ logger.setLevel(logging.INFO)
 # Create the agent
 agent = Agent('llm_agent')
 # Load agent properties stored in a dedicated file
-agent.load_properties('config_marcos.yaml')
+agent.load_properties('config.yaml')
 # Define the platform your agent will use
 websocket_platform = agent.use_websocket_platform(use_ui=True)
 
